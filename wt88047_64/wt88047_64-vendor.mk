324e773ec75e232b5f50533d928dad05d7d9f2ad
Nicholas Lim <nicknitewolf@lineageos.org>
2020-04-25 22:45:22 +0800

    wt88047: Remove unused camera firmware
    Signed-off-by: RahifM <faizel326@gmail.com>

--- wt88047_64/wt88047_64-vendor.mk
+++ wt88047_64/wt88047_64-vendor.mk
@@ -32,9 +32,6 @@ PRODUCT_COPY_FILES += \
 
 # Camera firmware - from wt88047
 PRODUCT_COPY_FILES += \
-    vendor/wingtech64/wt88047_64/proprietary/vendor/firmware/cpp_firmware_v1_1_1.fw:system/vendor/firmware/cpp_firmware_v1_1_1.fw \
-    vendor/wingtech64/wt88047_64/proprietary/vendor/firmware/cpp_firmware_v1_1_6.fw:system/vendor/firmware/cpp_firmware_v1_1_6.fw \
-    vendor/wingtech64/wt88047_64/proprietary/vendor/firmware/cpp_firmware_v1_2_0.fw:system/vendor/firmware/cpp_firmware_v1_2_0.fw \
     vendor/wingtech64/wt88047_64/proprietary/vendor/firmware/cpp_firmware_v1_4_0.fw:system/vendor/firmware/cpp_firmware_v1_4_0.fw
 
 
